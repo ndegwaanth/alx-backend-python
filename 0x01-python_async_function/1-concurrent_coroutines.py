79dd785726b069d01570568bb67b5e6da874ae2d
Anthony <ndegwaanthony300@gmail.com>
2024-07-09 00:00:23 +0300

    modified the import format of the 0-basic_async_syntax

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -2,7 +2,7 @@
 
 import asyncio
 from typing import List
-from basic_async_syntax import wait_random
+wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 async def wait_n(n: int, max_delay: int = 10) -> List[float]:
