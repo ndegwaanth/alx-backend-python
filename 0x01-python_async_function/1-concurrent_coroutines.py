ea3dc9de2e08aec0eb995a424ad88cf156b275e9
Anthony <ndegwaanthony300@gmail.com>
2024-07-09 00:14:52 +0300

    added a newline in this file 1-concurrent_coroutines.py

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -12,4 +12,5 @@ async def wait_n(n: int, max_delay: int = 10) -> List[float]:
         the specified max_delay
     """
     delays = await asyncio.gather(*(wait_random(max_delay) for _ in range(n)))
+
     return sorted(delays)
