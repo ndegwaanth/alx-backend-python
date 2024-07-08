eff4ecf8dfc0e59541b7ba9b62da33ae57ec127d
Anthony <ndegwaanthony300@gmail.com>
2024-07-09 00:46:30 +0300

    4-tasks.py

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-"""4-tasks.py"""
+"""tasks 4"""
 
 import asyncio
 from typing import List
@@ -8,10 +8,10 @@ wait_n = __import__('1-concurrent_coroutines').wait_n
 
 async def task_wait_n(n: int, max_delay: int = 10) -> List[float]:
     """
-    Take the code from wait_n and alter it into a
-    new function task_wait_n. The code is nearly
-    identical to wait_n except task_wait_random
-    is being called.
+        Take the code from wait_n and alter it into a
+        new function task_wait_n. The code is nearly
+        identical to wait_n except task_wait_random
+        is being called.
     """
     delays = await asyncio.gather(*(wait_n(max_delay) for _ in range(n)))
 
