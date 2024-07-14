73260de96f825e37c26c9666e5a7a6bd09844357
Anthony <ndegwaanthony300@gmail.com>
2024-07-14 21:08:33 +0300

    0-async_generator.py

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -2,10 +2,9 @@
 """0. Async Generator"""
 import asyncio
 import random
-import typing
 
 
-async def async_generator() -> typing.AsyncGenerator[float]:
+async def async_generator() -> float:
     """
         Coroutine that loops 10 times, each time
         asynchronously waits 1 second,
--- 0x02-python_async_comprehension/README.md
+++ 0x02-python_async_comprehension/README.md
@@ -1,1 +1,1 @@
-0x02-python_async_comprehension
+0x02-python_async_comprehensio
