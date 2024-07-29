cdf601a201cc01e6861132d62e702e6716e66ac0
Anthony <ndegwaanthony300@gmail.com>
2024-07-30 00:32:16 +0300

    initial commit

--- 0x03-Unittests_and_integration_tests/tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/tests/test_utils.py
@@ -5,12 +5,14 @@ from utils import access_nested_map, get_json, memoize
 
 
 class TestAccessNestedMap(unittest.TestCase):
+    """class testing access_nested_map function"""
     @parameterized.expand([
         ({"a": 1}, ("a",), 1),
         ({"a": {"b": 2}}, ("a",), {"b": 2}),
         ({"a": {"b": 2}}, ("a", "b"), 2),
     ])
     def test_access_nested_map(self, nested_map, path, expected):
+        """several test to one"""
         self.assertEqual(access_nested_map(nested_map, path), expected)
 
     @parameterized.expand([
@@ -18,17 +20,20 @@ class TestAccessNestedMap(unittest.TestCase):
         ({"a": 1}, ("a", "b")),
     ])
     def test_access_nested_map_exception(self, nested_map, path):
+        """tests exception raising in access_nested_map function"""
         with self.assertRaises(KeyError) as context:
             access_nested_map(nested_map, path)
         self.assertEqual(str(context.exception), f"'{path[-1]}'")
 
 
 class TestGetJson(unittest.TestCase):
+    """testimg get_json methos"""
     @parameterized.expand([
         ("http://example.com", {"payload": True}),
         ("http://holberton.io", {"payload": False}),
     ])
     def test_get_json(self, test_url, test_payload):
+        """Test"""
         with patch('utils.requests.get') as mock_get:
             mock_response = Mock()
             mock_response.json.return_value = test_payload
@@ -41,25 +46,22 @@ class TestGetJson(unittest.TestCase):
 
 
 class TestMemoize(unittest.TestCase):
+    """memoisation testing"""
     class TestClass:
         def a_method(self):
             return 42
 
         @memoize
         def a_property(self):
+            '''not cached'''
             return self.a_method()
 
     @patch.object(TestClass, 'a_method', return_value=42)
     def test_memoize(self, mock_a_method):
+        """Use unittest.mock.patch to mock a_method"""
         test_instance = self.TestClass()
-
-        # Call the memoized property twice
         result1 = test_instance.a_property
         result2 = test_instance.a_property
-
-        # Check that the method was called only once
         mock_a_method.assert_called_once()
-
-        # Check that the results are correct
         self.assertEqual(result1, 42)
         self.assertEqual(result2, 42)
